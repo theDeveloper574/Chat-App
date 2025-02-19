633870f33d6c5fe95de862991bcf9b4ee14644c1
Nauman Yameen <92442595+theDeveloper574@users.noreply.github.com>
2025-02-19 15:26:28 +0500

    Delete Chat-backend directory

--- Chat-backend/src/tokens/token_generation.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-
-module.exports.createSecretToken = (id) => {
-    return jwt.sign({ id }, process.env.TOKEN_KEY, {
-        expiresIn: "3d",
-        // expiresIn: 3 * 24 * 60 * 60,
-    });
-};
